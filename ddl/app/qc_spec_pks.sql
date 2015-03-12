275e33da248fc3bb60b442d6d1d2afe841a17daf
Mathias Magnusson <ext.mathias.magnusson@apotekensservice.se>
2015-03-12 16:23:48 +0100

    Lagt till datadomän, trigger, och spec på paket

---
+++ ddl/app/qc_spec_pks.sql
@@ -0,0 +1,3 @@
+create or replace package qc_spec authid definer is
+  min_number_words constant number := 2;
+end qc_spec;
---
+++ ddl/db/qc_trg.sql
@@ -0,0 +1,17 @@
+create or replace trigger qc
+  before create on database
+  when(ora_dict_obj_owner = 'MU'
+   and ora_dict_obj_type  = 'TABLE')
+
+  declare
+    v_min_number_words number := mu.qc_spec.min_number_words;
+    
+  begin
+    
+  
+    if regexp_count(ora_dict_obj_name, '_') + 1 < v_min_number_words then
+      raise_application_error (
+        num => -20000,
+        msg => 'Cannot create object');
+    end if;
+  end;
